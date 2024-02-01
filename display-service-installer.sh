9e0d94e8e8eee797604b0c0c12874e11ed65f816
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-01 21:12:33 +0000

    Branch agnostic moonraker update manager entry

--- display-service-installer.sh
+++ display-service-installer.sh
@@ -13,7 +13,11 @@ VENV_PATH="$HOME/display_connector/venv"
 LOG_FILE="/var/log/display.log"
 MOONRAKER_ASVC="$HOME/printer_data/moonraker.asvc"
 
-current_branch=$(git -C "$HOME/display_connector" branch --show-current 2>/dev/null)
+# Check if the script exists
+if [ ! -f "$SCRIPT_PATH" ]; then
+    echo "Error: Script $SCRIPT_PATH not found."
+    exit 1
+fi
 
 # Check if the old service exists and is running
 if systemctl is-active --quiet OpenNept4une; then
@@ -69,4 +73,28 @@ sudo systemctl start display.service
 echo "Allowing Moonraker to control display service"
 grep -qxF 'display' $MOONRAKER_ASVC || echo 'display' >> $MOONRAKER_ASVC
 
-sudo service moonraker restart
+current_branch=$(git -C "$HOME/display_connector" branch --show-current 2>/dev/null)
+
+# Define the lines to be inserted or updated
+new_lines="[update_manager display]\n\
+type: git_repo\n\
+primary_branch: $current_branch\n\
+path: ~/display_connector\n\
+virtualenv: ~/display_connector/venv\n\
+origin: https://github.com\/OpenNeptune3D/display.git"
+
+# Define the path to the moonraker.conf file
+config_file="$HOME/printer_data/config/moonraker.conf"
+
+# Check if the lines exist in the config file
+if grep -qF "[update_manager display]" "$config_file"; then
+    # Lines exist, update them
+    perl -pi.bak -e "BEGIN{undef $/;} s|\[update_manager display\].*?((?:\r*\n){2}\|$)|$new_lines\$1|gs" "$config_file"
+else
+    # Lines do not exist, append them to the end of the file
+    echo -e "\n$new_lines" >> "$config_file"
+fi
+
+echo "Service setup complete."
+
+sudo service moonraker restart 
