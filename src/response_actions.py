973550e7bda7102a72f2d76ce00e385243ce804e
Phillip Thelen <phillip@thelen.space>
2024-02-13 11:01:31 +0100

    Fix setting temp presets on some models

--- src/response_actions.py
+++ src/response_actions.py
@@ -1,12 +1,14 @@
 from src.mapping import *
 
 response_actions = {
+    # Main
     1: {
         1: "files_picker",
         2: "page " + PAGE_PREPARE_MOVE,
         3: "page " + PAGE_SETTINGS,
         4: "page " + PAGE_LEVELING,
     },
+    #File Picker
     2: {
         2: "files_page_next",
         1: "files_page_prev",
@@ -16,10 +18,12 @@ response_actions = {
         10: "open_file_3",
         11: "open_file_4",
     },
+    #Level Picker
     3: {
         7: "page " + PAGE_LEVELING_SCREW_ADJUST,
         8: "page " + PAGE_LEVELING_Z_OFFSET_ADJUST,
     },
+    #Prepare Temperature (Pro Only)
     6: {
         1: "printer.send_gcode('SET_HEATER_TEMPERATURE HEATER=extruder')",
         2: "printer.send_gcode('SET_HEATER_TEMPERATURE HEATER=heater_bed')",
@@ -31,6 +35,7 @@ response_actions = {
         8: "page " + PAGE_PREPARE_EXTRUDER,
         9: "printer.send_gcode('SET_HEATER_TEMPERATURE HEATER=heater_bed_outer')",
     },
+    # Prepare Move
     8: {
         1: 'set_distance_0.1',
         2: 'set_distance_1',
@@ -49,12 +54,14 @@ response_actions = {
         15: 'page ' + PAGE_PREPARE_TEMP,
         16: 'page ' + PAGE_PREPARE_EXTRUDER,
     },
+    # Prepare Extruder
     9: {
         1: 'extrude_+',
         2: 'extrude_-',
         3: "page " + PAGE_PREPARE_MOVE,
         4: "page " + PAGE_PREPARE_TEMP,
     },
+    # Settings
     11: {
         1: "page " + PAGE_SETTINGS_LANGUAGE,
         2: "page " + PAGE_SETTINGS_TEMPERATURE,
@@ -65,10 +72,12 @@ response_actions = {
         8: "page " + PAGE_SETTINGS_ABOUT,
         9: "page " + PAGE_SETTINGS_ADVANCED,
     },
+    # Confirm Print
     18: {
         0: 'print_opened_file',
         1: 'go_back'
     },
+    #Printing
     19: {
         0: "page " + PAGE_PRINTING_FILAMENT,
         1: 'pause_print_button',
@@ -78,18 +87,22 @@ response_actions = {
         5: "page " + PAGE_PRINTING_DIALOG_FLOW,
         6: "page " + PAGE_PRINTING_DIALOG_SPEED,
     },
+    # Print Completed
     24: {
         0: 'confirm_complete',
         1: 'print_opened_file',
     },
+    # Confirm Pause
     25: {
         0: "pause_print_confirm",
         1: 'go_back'
     },
+    # Confirm Stop
     26: {
         0: "stop_print",
         1: 'go_back'
     },
+    # Printing Temp (4 Pro only)
     27: {
         1: 'temp_heater_extruder',
         2: 'temp_heater_heater_bed',
@@ -103,6 +116,7 @@ response_actions = {
         12: "page " + PAGE_PRINTING_SPEED,
         13: "page " + PAGE_PRINTING_ADJUST
     },
+    # Printing Temp
     28: {
         1: 'temp_heater_extruder',
         2: 'temp_heater_heater_bed',
@@ -115,12 +129,14 @@ response_actions = {
         12: "page " + PAGE_PRINTING_SPEED,
         13: "page " + PAGE_PRINTING_ADJUST
     },
+    # Settings Temperature
     32: {
         1: "start_temp_preset_pla",
         2: "start_temp_preset_abs",
         3: "start_temp_preset_petg",
         4: "start_temp_preset_tpu",
     },
+    # Settings Temperature Set
     33: {
         0: 'save_temp_preset',
         1: 'preset_temp_step_1',
@@ -131,30 +147,36 @@ response_actions = {
         6: "preset_temp_bed_down",
         7: "preset_temp_bed_up",
     },
+    # Lights Page
     84: {
         1: "toggle_part_light",
         2: "toggle_frame_light",
     },
+    # Printing Speed
     86: {
         1: '',
     },
+    # Leveling Screws
     94: {
         5: 'retry_screw_leveling'
     },
+    # Prepare Temperature
     95: {
         1: "printer.send_gcode('SET_HEATER_TEMPERATURE HEATER=extruder')",
         2: "printer.send_gcode('SET_HEATER_TEMPERATURE HEATER=heater_bed')",
-        3: 'preset_temp_PLA',
-        4: 'preset_temp_ABS',
-        5: 'preset_temp_PETG',
-        6: 'preset_temp_TPU',
+        3: 'set_preset_temp_PLA',
+        4: 'set_preset_temp_ABS',
+        5: 'set_preset_temp_PETG',
+        6: 'set_preset_temp_TPU',
         7: "page " + PAGE_PREPARE_MOVE,
         8: "page " + PAGE_PREPARE_EXTRUDER,
     },
+    # Confirm Emergency Stop
     106: {
         0: "emergency_stop",
         1: 'go_back'
     },
+    # Printing Adjust
     127: {
         1: 'zoffsetchange_0.01',
         2: 'zoffsetchange_0.1',
@@ -164,6 +186,7 @@ response_actions = {
         7: 'page ' + PAGE_LIGHTS,
         8: 'toggle_filament_sensor'
     },
+    #Printing Speed
     135: {
         1: 'speed_type_print',
         2: 'speed_type_flow',
@@ -175,6 +198,7 @@ response_actions = {
         8: 'speed_adjust_+',
         9: 'speed_reset',
     },
+    # Leveling Z Offset
     137: {
         0: 'abort_zprobe',
         1: 'zprobe_step_0.01',
@@ -187,21 +211,26 @@ response_actions = {
 }
 
 input_actions = {
+    # Prepare Temp (4 Pro only)
     6: {
         0: "set_temp_extruder_$",
         1: "set_temp_heater_bed_$",
         2: "set_temp_heater_bed_outer_$",
     },
+    # Prepare Extruder
     9: {
         2: "set_extrude_amount_$",
         3: "set_extrude_speed_$",
     },
+    # Printing Temperature
     85: {
         1: "set_flow_$",
     },
+    # Printing Speed
     86: {
         1: "set_speed_$",
     },
+    # Prepare Temp
     95: {
         0: "set_temp_extruder_$",
         1: "set_temp_heater_bed_$",
