ea8c787d9e8c0c36cede095eee2fc5608ceb5b61
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-02-18 21:02:13 +0000

    Wifi Symbology Fix

--- src/wifi.py
+++ src/wifi.py
@@ -17,22 +17,25 @@ def get_wlan0_status():
         rssi = None
         for line in rssi_output.strip().split('\n'):
             if line.startswith('*:'):
-                rssi = line.split(':')[1]
+                rssi = int(line.split(':')[1])  # Convert the string to an integer
                 break
 
-        return True, ssid, rssi
+        # Categorize the signal strength
+        rssi_category = categorize_signal_strength(rssi)
+
+        return True, ssid, rssi_category
 
     except subprocess.CalledProcessError:
         return False, None, None
 
-def categorize_signal_strength(signal_dbm):
-    if signal_dbm is None:
+def categorize_signal_strength(signal_percentage):
+    if signal_percentage is None:
         return 0
-    elif signal_dbm < -70:
+    elif signal_percentage < 25:
         return 1
-    elif -70 <= signal_dbm < -60:
+    elif 25 <= signal_percentage < 50:
         return 2
-    elif -60 <= signal_dbm < -50:
+    elif 50 <= signal_percentage < 75:
         return 3
     else:
         return 4
