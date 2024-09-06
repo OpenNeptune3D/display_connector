07c3c8868ac1e2927370c1c411b1d77c6a0afd76
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2024-09-06 16:10:01 +0100

    Update elegoo_custom.py
    addressing circular dependency issue

--- src/elegoo_custom.py
+++ src/elegoo_custom.py
@@ -1,11 +1,9 @@
+import importlib
 from logging import Logger
-
-from src.elegoo_display import ElegooDisplayCommunicator, ElegooDisplayMapper
 
 MODEL_CUSTOM = "Custom"
 
-
-class CustomDisplayCommunicator(ElegooDisplayCommunicator):
+class CustomDisplayCommunicator:
     def __init__(
         self,
         logger: Logger,
@@ -15,5 +13,9 @@ class CustomDisplayCommunicator(ElegooDisplayCommunicator):
         baudrate: int = 115200,
         timeout: int = 5,
     ) -> None:
+        # Dynamically import ElegooDisplayCommunicator and ElegooDisplayMapper to avoid circular import
+        ElegooDisplayCommunicator = importlib.import_module('src.elegoo_display').ElegooDisplayCommunicator
+        ElegooDisplayMapper = importlib.import_module('src.elegoo_display').ElegooDisplayMapper
+        
         super().__init__(logger, model, port, event_handler, baudrate, timeout)
         self.mapper = ElegooDisplayMapper()
