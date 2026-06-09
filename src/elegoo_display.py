64b4bae51c077c6fb6f517199d5cc6841d4cd50b
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2026-06-09 13:59:39 +0100

    Remove commented-out code in elegoo_display.py

--- src/elegoo_display.py
+++ src/elegoo_display.py
@@ -412,6 +412,7 @@ class ElegooDisplayCommunicator(DisplayCommunicator):
         elif current_page == PAGE_LIGHTS:
             #await self.write("t0.txt=\"Part Light\"")
             #await self.write("t1.txt=\"Frame Light\"")
+            pass
         elif current_page == PAGE_PRINTING:
             await self.write("printvalue.xcen=0")
             await self.write("move printvalue,13,267,13,267,0,10")
