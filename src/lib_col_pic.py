083765dc609d7d6a2aab707c8c393f68a416d29b
HalfManBear <89969229+halfmanbear@users.noreply.github.com>
2026-06-09 20:37:42 +0100

    Optimize outputdata initialization and return statement
    Removed unnecessary initialization of outputdata and fixed return statement formatting.

--- src/lib_col_pic.py
+++ src/lib_col_pic.py
@@ -171,8 +171,6 @@ def _colpic_encode(
     if HEADER_SIZE + list_data_size + 2 > outputmaxsize:
         return 0
 
-    outputdata[:outputmaxsize] = bytes(outputmaxsize)
-
     outputdata[0] = 3
     outputdata[4:8] = int(picw).to_bytes(4, 'little')
     outputdata[8:12] = int(pich).to_bytes(4, 'little')
